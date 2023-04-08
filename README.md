24fb6fa26ef1d7371c392c92bdacef8723597c63
JosueDilmo <josuedilmo@gmail.com>
2023-04-08 18:31:02 +0100

    features and authentication

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # Flask TODO App
 This is a Flask application that allows users to create and delete TODO items. <br>
-The application uses MongoDB Atlas to store the TODO items.
+The application uses MongoDB Atlas to store the TODO items and USERS.
+To use the application, users must first register by providing a username and password.<br>
+The password is hashed using Flask-Bcrypt before being stored in the database for security. <br>
+After registering, users can log in using their username and password to access their todo list.
+
 
 ## Installation
 To run the application, you will need to install Python 3.x and the following packages:
 
 - Flask
 - pymongo
+- bcrypt
+
 You can install the packages using pip.
 
 bash <br>
-`pip install flask pymongo bson`
+`pip install flask pymongo bson flask-bcrypt`
 
 # Configuration
 The application uses MongoDB Atlas as the database. <br> 
@@ -18,37 +24,41 @@ You will need to create a database on MongoDB Atlas and obtain a connection stri
 Update the client variable in app.py with your connection string.<br>
 
 python<br>
-``client = MongoClient('mongodb+srv://<username>:<password>@cluster0.kjtwkrj.mongodb.net/<database>?retryWrites=true&w=majority')`
+`client = MongoClient('mongodb+srv://<username>:<password>@cluster0.kjtwkrj.mongodb.net/?retryWrites=true&w=majority')`
 
 # Usage
 To run the application, execute the following command in the terminal:
 
 bash
-`python app.py`
+`python app.py` <br>
 The application will be available at http://localhost:5000. <br>
+You can create an account using username and password.
+You can login to your account and access your TODO items.
 You can create new TODO items by entering the content and selecting the degree of importance.<br>
 You can delete TODO items by clicking on the delete button next to each item.<br>
 
 ## Application running:
-
+<i>I also added a video at ./media/todoApp.mp4.</i>
 <p align="center">
-  <img alt="pic1" src="./screens/pic1.png" >
+  <img alt="pic1" src="./media/pic1.png" >
 </p>
 
 <p align="center">
-  <img alt="pic2" src="./screens/pic2.png" >
+  <img alt="pic2" src="./media/pic2.png" >
 </p>
 
 <p align="center">
-  <img alt="pic3" src="./screens/pic3.png" >
+  <img alt="pic3" src="./media/pic3.png" >
 </p>
 
 <p align="center">
-  <img alt="pic4" src="./screens/pic41.png" >
+  <img alt="pic4" src="./media/pic4.png" >
 </p>
 
 
 ## References
 - [How To Use MongoDB in a Flask Application](https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application)
+- [Flask-Bcrypt](https://flask-bcrypt.readthedocs.io/en/1.0.1/)
 - [Bootstrap](https://getbootstrap.com/docs/5.3/getting-started/introduction/)
 - [Flask video](https://www.youtube.com/watch?v=xjHEcmjlD-Y&t=493s)
+- [Flask Auth video](https://www.youtube.com/watch?v=71EU8gnZqZQ)
--- app.py
+++ app.py
@@ -1,39 +1,130 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from flask_bcrypt import Bcrypt
+from functools import wraps
 
-
-# instance for flask
+# Creating an instance of Flask
 app = Flask(__name__)
 
-# database in Atlas MongoDB
+# Creating an instance of Flask_Bcrypt
+bcrypt = Bcrypt(app)
+
+# Setting up the MongoDB database connection
 client = MongoClient('mongodb+srv://admin:Password1@cluster0.kjtwkrj.mongodb.net/?retryWrites=true&w=majority')
 
-# Variables
+# Setting up the database and collections
 db = client.mydb
 todos = db.todos
+users = db.users
+
+# Setting up the app secret key for sessions
+app.secret_key = 'Something very secret!'
 
 
-# Post new todos on the database and retrieve all todos from the database
-# Index is using the index.html template
-@app.route('/', methods=('GET', 'POST'))
+# POST new todos on the database and retrieve all todos from the database.
+# Index is using the index.html template.
+@app.route('/todo', methods=('GET', 'POST'))
 def index():
-    if request.method=='POST':
+    if request.method == 'POST':
+        # Get the content, degree of priority, and due date from the form
         content = request.form['content']
         degree = request.form['degree']
-        todos.insert_one({'content': content, 'degree': degree})
+        due_date = request.form['due_date']
+
+        # Insert the new todos into the todos collection
+        todos.insert_one({'content': content, 'degree': degree, 'due_date': due_date})
+
+        # Redirect to the index page to show the updated list of todos
         return redirect(url_for('index'))
 
+    # Find all todos in the database
     all_todos = todos.find()
+
+    # Render the index.html template with the todos data
     return render_template('index.html', todos=all_todos)
 
 
-# Delete the todos on the Atlas database
+# Delete the todo with the given id from the Atlas database.
 @app.post('/<id>/delete/')
 def delete(id):
+    # Delete the todos from the todos collection using its ID
     todos.delete_one({"_id": ObjectId(id)})
+
+    # Redirect to the index page to show the updated list of todos
     return redirect(url_for('index'))
 
 
+# Login required decorator to restrict access to certain pages.
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('user') is None:
+            # Redirect the user to the login page if they're not logged in
+            return redirect(url_for('login', next=request.url))
+
+        # Call the original function if the user is logged in
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+# Login page when "localhost" is accessed.
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get the username and password from the login form
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Find the user in the users collection
+        user = users.find_one({'username': username})
+
+        if user is None:
+            # Return an error message if the username doesn't exist in the database
+            return 'Invalid username or password'
+
+        if bcrypt.check_password_hash(user['password'], password):
+            # Set the user session if the password is correct
+            session['user'] = username
+            # Redirect to the index page to show the list of todos
+            return redirect(url_for('index'))
+        # Return an error message if the password is incorrect
+        return 'Invalid username or password'
+    # Render the login.html template if the request is a GET request
+    return render_template('login.html')
+
+
+# Template render for the registration form
+@app.route('/register')
+def register_form():
+    return render_template('register.html')
+
+
+# Register user by username and password
+# handle the submission of the registration form
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # check if the username is already registered
+    user_registered = users.find_one({'username': username})
+
+    # if the users does not exit, create it
+    if user_registered is None:
+        passwordHash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        # add document to the database
+        users.insert_one({'username': username, 'password': passwordHash})
+
+        # redirect to login page
+        return redirect(url_for('login'))
+
+    # if username is already registered, do not register
+    return 'Username already registered!'.encode('utf-8')
+
+
+# run app in debug mode
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
